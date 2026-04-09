c3134d9e7da070c93c4750b1a0b396f577a4f231
ARSzed <azaitov40@gmail.com>
2026-04-09 22:57:56 +0300

    fifth commit

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 class Main {
     public static void main(String[] args) {
-        String message = "Привет от Git!";
+        String message = getMessage();
         System.out.print("Это добавлено в ветке main");
-        System.out.println(message);
+        System.out.println(" - " + message);
+    }
+
+    static String getMessage() {
+        return "Привет от Git!";
     }
 }
