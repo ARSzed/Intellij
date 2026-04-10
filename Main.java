0fd2dcee892d3d22379bfc9213c898917c5e8204
ARSzed <azaitov40@gmail.com>
2026-04-10 10:48:04 +0300

    dev: first change Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
+
+        String[] names = {"Анна", "Борис", "Виктор", "Галина"};
+
+        System.out.println("\nСписок пользователей:");
+        for (String name : names) {
+            System.out.println("* " + name);
+        }
     }
 }
